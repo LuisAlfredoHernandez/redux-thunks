77be61a9dba08dbb355904a3b838edcf6a1e9dbd
Luis Hernández <lhernandez@contractor.appsbees.com>
2023-01-21 21:53:04 -0400

    firs commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux'
+import mithunks from './thunk'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    const { mithunks } = props
+    mithunks('lala')
+    console.log(props)
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
+const mapStateToProps = state => state
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  mithunks: payload => dispatch(mithunks(payload))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
